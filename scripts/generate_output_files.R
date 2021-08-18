5d7fb55055e6b6d35b205368afec06eb35db51b0
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-08-18 12:32:42 -0500

    update Rscript to include library ID

--- scripts/generate_output_files.R
+++ scripts/generate_output_files.R
@@ -9,17 +9,17 @@ library(scpcaTools)
 # set up arguments 
 option_list <- list(
   make_option(
-    opt_str = c("-r", "--run_id"),
+    opt_str = c("-l", "--library_id"),
     type = "character",
-    help = "scpca_run_id"
+    help = "scpca_library_id"
   ), 
   make_option(
-    opt_str = c("-c", "--which_counts"),
+    opt_str = c("-s", "--seq_unit"),
     type = "character", 
     help = "include counts for spliced cDNA only (spliced) or unspliced and spliced cDNA (unspliced)"
   ), 
   make_option(
-    opt_str = c("-o", "-output_dir"),
+    opt_str = c("-o", "--output_dir"),
     type = "character", 
     help = "output location for sce file"
   )
@@ -28,40 +28,30 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list = option_list))
 
 # check arguments are correct 
-if(is.null(opt$run_id)){
-  stop("Must input a scpca_run_id.")
+if(is.null(opt$library_id)){
+  stop("Must input a scpca_library_id.")
 }
 
 # check for compatible sequencing unit types 
-if(!(opt$which_counts %in% c("spliced", "unspliced"))){
+if(!(opt$seq_unit %in% c("cell", "nucleus"))){
   stop("Sequencing unit must be of type cell or nucleus")
 }
 
-# set up file directories 
-# location where processed alevin-fry files live 
-s3_processed_files <- 's3://nextflow-ccdl-scpca/scpca/processed'
-sample_dir <- paste(file.path(s3_processed_files, opt$run_id), sep = "")
+# set up filenames
+unfiltered_sce_file <- file.path(opt$output_dir, "unfiltered_sce.rds")
+filtered_sce_file <- file.path(opt$output_dir, "filtered_sce.rds")
 
-s3_output_files <- 's3://nextflow-ccdl-scpca/scpca/sce_processed'
-sample_output_dir <- file.path(s3_output_files, opt$run_id)
-
-unfiltered_sce_file <- file.path(sample_output_dir, "unfiltered_sce.rds")
-filtered_sce_file <- file.path(sample_output_dir, "filtered_sce.rds")
-
-if(!dir.exists(sample_output_dir)){
-  dir.create(sample_output_dir, recursive = TRUE)
-}
-
+which_counts <- dplyr::case_when(opt$seq_unit == "cell" ~ "spliced",
+                                 opt$seq_unit == "nucleus" ~ "unspliced")
 
 # get filtered and unfiltered sce 
-# we may also want to eventually add in per cell QC metrics here 
-unfiltered_sce <- import_quant_data(quant_dir = file.path(base_dir, sample), 
+unfiltered_sce <- import_quant_data(quant_dir = opt$output_dir, 
                                     tool = "alevin-fry", 
-                                    which_counts = opt$which_counts, 
+                                    which_counts = opt$seq, 
                                     usa_mode = TRUE)
 
 
-filtered_sce <- import_quant_data(quant_dir = file.path(base_dir, sample), 
+filtered_sce <- import_quant_data(quant_dir = opt$output_dir, 
                                     tool = "alevin-fry", 
                                     which_counts = opt$which_counts, 
                                     usa_mode = TRUE, 
