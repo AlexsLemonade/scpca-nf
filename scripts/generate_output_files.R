49f447564b475c3f1bbbade0dc67208ea20d7147
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-08-18 15:37:09 -0500

    update arguments in Rscript

--- scripts/generate_output_files.R
+++ scripts/generate_output_files.R
@@ -9,55 +9,62 @@ library(scpcaTools)
 # set up arguments 
 option_list <- list(
   make_option(
-    opt_str = c("-l", "--library_id"),
-    type = "character",
-    help = "scpca_library_id"
-  ), 
-  make_option(
     opt_str = c("-s", "--seq_unit"),
     type = "character", 
     help = "include counts for spliced cDNA only (spliced) or unspliced and spliced cDNA (unspliced)"
   ), 
   make_option(
-    opt_str = c("-o", "--output_dir"),
+    opt_str = c("-a", "--alevin_dir"),
     type = "character", 
-    help = "output location for sce file"
+    help = "directory with alevin output files"
+  ),
+  make_option(
+    opt_str = c("-u", "--unfiltered_file"),
+    type = "character",
+    help = "path to output unfiltered rds file. Must end in .rds"
+  ), 
+  make_option(
+    opt_str = c("-f", "--filtered_file"),
+    type = "character", 
+    default = "filtered",
+    help = "path to output unfiltered rds file. Must end in .rds"
   )
 )
 
 opt <- parse_args(OptionParser(option_list = option_list))
-
-# check arguments are correct 
-if(is.null(opt$library_id)){
-  stop("Must input a scpca_library_id.")
-}
 
 # check for compatible sequencing unit types 
 if(!(opt$seq_unit %in% c("cell", "nucleus"))){
   stop("Sequencing unit must be of type cell or nucleus")
 }
 
-# set up filenames
-unfiltered_sce_file <- file.path(opt$output_dir, "unfiltered_sce.rds")
-filtered_sce_file <- file.path(opt$output_dir, "filtered_sce.rds")
+# check that output file names end in .rds 
+if(!(stringr::str_ends(opt$unfiltered_file, ".rds"))){
+  stop("unfiltered file name must end in .rds")
+}
 
+if(!(stringr::str_ends(opt$filtered_file, ".rds"))){
+  stop("filtered file name must end in .rds")
+}
+
+# convert seq_unit to spliced or unspliced to determine which types of transcripts to include in final counts matrix
 which_counts <- dplyr::case_when(opt$seq_unit == "cell" ~ "spliced",
                                  opt$seq_unit == "nucleus" ~ "unspliced")
 
 # get filtered and unfiltered sce 
-unfiltered_sce <- import_quant_data(quant_dir = opt$output_dir, 
+unfiltered_sce <- import_quant_data(quant_dir = opt$alevin_dir, 
                                     tool = "alevin-fry", 
-                                    which_counts = opt$seq, 
+                                    which_counts = which_counts, 
                                     usa_mode = TRUE)
 
 
-filtered_sce <- import_quant_data(quant_dir = opt$output_dir, 
+filtered_sce <- import_quant_data(quant_dir = opt$alevin_dir, 
                                     tool = "alevin-fry", 
-                                    which_counts = opt$which_counts, 
+                                    which_counts = which_counts, 
                                     usa_mode = TRUE, 
                                     filter = TRUE)
 
 # write files 
-readr::write_rds(unfiltered_sce, unfiltered_sce_file)
-readr::write_rds(filtered_sce, filtered_sce_file)
+readr::write_rds(unfiltered_sce, opt$unfiltered_file)
+readr::write_rds(filtered_sce, opt$filtered_file)
 
