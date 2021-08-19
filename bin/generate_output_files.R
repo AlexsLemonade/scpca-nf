0df95ca8450a7e6febfeff9119ccf9aa3f1a1da4
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-08-19 09:41:57 -0500

    remove shebang

--- bin/generate_output_files.R
+++ bin/generate_output_files.R
@@ -1,5 +1,3 @@
-#!/usr/bin/Rscript
-
 # This script takes the output folder from alevin-fry as input and 
 # returns both the filtered and unfiltered counts matrices as a SingleCellExperiment stored in a .rds file 
 
