39acf116530605a6b2e7a34066713f2956fb84de
Ally Hawkins <ally.hawkins@ccdatalab.org>
2021-09-09 13:13:34 -0500

    add coldata and rowdata after filtering

--- bin/filter_sce_rds.R
+++ bin/filter_sce_rds.R
@@ -18,6 +18,18 @@ option_list <- list(
     opt_str = c("-f", "--filtered_file"),
     type = "character",
     help = "path to output filtered rds file. Must end in .rds"
+  ),
+  make_option(
+    opt_str = c("-m", "--mito_file"),
+    type = "character",
+    default = "",
+    help = "path to list of mitochondrial genes"
+  ), 
+  make_option(
+    opt_str = c("-n", "--feature_name"),
+    type = "character",
+    default = "ALT",
+    help = "Feature type"
   )
 )
 
@@ -38,6 +50,24 @@ unfiltered_sce <- readr::read_rds(opt$unfiltered_file)
 
 # filter sce
 filtered_sce <- scpcaTools::filter_counts(unfiltered_sce)
+
+# need to remove old colData and rowData first 
+colData(filtered_sce) <- NULL
+rowData(filtered_sce) <- NULL
+
+# add colData and rowData to filtered sce 
+filtered_sce <- filtered_sce %>%
+  scpcaTools::add_cell_mito_qc(mito = mito_genes, miQC = TRUE) %>%
+  scater::addPerFeatureQC()
+
+# if altExp is present, add feature data
+if (opt$feature_name != "") {
+  # remove old row data from unfiltered 
+  rowData(altExp(filtered_sce, opt$feature_name)) <- NULL
+  
+  # add alt experiment features stats for filtered data
+  altExp(filtered_sce, opt$feature_name) <- scater::addPerFeatureQC(altExp(filtered_sce, opt$feature_name))
+}
 
 # write filtered sce to output
 readr::write_rds(filtered_sce, opt$filtered_file, compress = "gz")
