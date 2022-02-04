c84a6623373ce6b720fdf84fae92307c62969ce5
Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
2022-02-04 11:41:56 -0600

    Apply suggestions from code review - add comment
    Co-authored-by: Joshua Shapiro <josh.shapiro@ccdatalab.org>

--- bin/generate_bulk_metadata.R
+++ bin/generate_bulk_metadata.R
@@ -112,7 +112,7 @@ get_processing_info <- function(library_id) {
   meta_info <- jsonlite::read_json(meta_info_file)
   
   date_processed <- lubridate::as_datetime(meta_info$end_time, 
-                                           format = "%a %b %d %T %Y")
+                                           format = "%a %b %d %T %Y") // salmon date format: like "Mon Jan 03 15:13:14 2022"
   # if meta_info is not recorded or the format has changed, use the modification time of the file
   if (is.na(date_processed)){
     date_processed <- file.info(meta_info_file)$mtime
