a7b4365c10e8c3fe969af01e65aa0ad50363878b
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-09-29 09:06:49 -0400

    Document utils

--- lib/Utils.groovy
+++ lib/Utils.groovy
@@ -1,22 +1,42 @@
-// Groovy functions used in multiple modules/processes
 
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
-
+/**
+ * Utility functions for scpca-nf
+ */
 class Utils {
+
+   /**
+     * Read a metadata file in JSON format
+     *
+     * @param file A Nextflow file object with metadata in JSON format
+     * @return Metadata as a groovy Map object
+     */
   static def readMeta(file) {
     def meta = new JsonSlurper().parse(file)
     return(meta)
   }
 
+  /**
+   * Write a metadata map to a JSON string
+   *
+   * @param meta A metadata map
+   * @return A JSON string
+   */
   static def makeJson(meta) {
     def meta_json = JsonOutput.toJson(meta)
     meta_json = JsonOutput.prettyPrint(meta_json)
     return(meta_json)
   }
 
-  static def getJsonVal(file, key){
+  /**
+   * Read an element from a metadata file in JSON format
+   *
+   * @param file A Nextflow file object with metadata in JSON format
+   * @return A value from the metadata
+   */
+  static def getMetaVal(file, key){
     def obj = new JsonSlurper().parse(file)
     return(obj[key])
   }
