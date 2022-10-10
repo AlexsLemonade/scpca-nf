fe82df4fa51c06628e2ed04c79781ec4ff6d5012
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-10-10 15:31:20 -0400

    set prob compromised cutoff to NA when miQC not used

--- bin/post_process_sce.R
+++ bin/post_process_sce.R
@@ -83,6 +83,7 @@ if(all(is.na(sce$prob_compromised))){
     sce$detected >= opt$gene_cutoff, "Keep", "Remove"
   )
   metadata(sce)$ccdl_filter_method <- "Minimum_gene_cutoff"
+  metadata(sce)$prob_compromised_cutoff <- NA_character_
 } else {
   # remove cells with >= probability compromised cutoff + min gene cutoff
   colData(sce)$ccdl_filter <- ifelse(
