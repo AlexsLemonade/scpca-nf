94f02267931ea9c15a7a72d01fc0bcddb52888b0
Ally Hawkins <ally.hawkins@ccdatalab.org>
2022-11-21 16:20:41 -0600

    fix argument name

--- bin/merge_sces.R
+++ bin/merge_sces.R
@@ -29,14 +29,24 @@ option_list <- list(
 # Parse options
 opt <- parse_args(OptionParser(option_list = option_list))
 
+# check that file extension for output file is correct 
+
 # list of paths to salmon files 
-input_sce_files <- stringr::str_split(opt$infiles, ',')
+input_sce_files <- stringr::str_split(opt$input_sce_files, ',')
 # pull library ids from list of 
 input_library_ids <- stringr::str_split(opt$library_ids, ',')
+
+# check that input files exist 
 
 # get list of sces
 sce_list <- purrr::map(input_sce_files, readr::read_rds)
 names(sce_list) <- input_library_ids
+
+# create combined SCE object with function in scpcaTools 
+
+# HVG calculation (from perform_hvg_selection in integration-helpers)
+
+# Dim Reduction PCA and UMAP 
 
 # write out merged sce file 
 readr::write_rds(sce_list, opt$output_sce_file)
