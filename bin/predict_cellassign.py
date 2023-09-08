1d9511476a58bf6faef45f7d5f9071a06df19369
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-09-08 11:55:43 -0400

    format python & config files

--- bin/predict_cellassign.py
+++ bin/predict_cellassign.py
@@ -15,28 +15,43 @@ import argparse
 import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input_hdf5_file',
-                    dest = 'input_hdf5_file',
-                    required = True,
-                    help = 'Path to HDF5 file with processed AnnData object to annotate')
-parser.add_argument('-o', '--output_predictions',
-                    dest = 'output_predictions',
-                    required = True,
-                    help = 'Path to file to save the predictions, should end in tsv')
-parser.add_argument('-r', '--reference',
-                    dest = 'reference',
-                    required = True,
-                    help = 'Path to marker by cell type reference file, should end in tsv')
-parser.add_argument('-s', '--seed',
-                    dest = 'seed',
-                    type=int,
-                    default = None,
-                    help = 'Random seed to set for scvi.')
-parser.add_argument('-t', '--threads',
-                    dest = 'threads',
-                    default = 1,
-                    type = int,
-                    help = 'Number of threads to use when training the CellAssign model')
+parser.add_argument(
+    "-i",
+    "--input_hdf5_file",
+    dest="input_hdf5_file",
+    required=True,
+    help="Path to HDF5 file with processed AnnData object to annotate",
+)
+parser.add_argument(
+    "-o",
+    "--output_predictions",
+    dest="output_predictions",
+    required=True,
+    help="Path to file to save the predictions, should end in tsv",
+)
+parser.add_argument(
+    "-r",
+    "--reference",
+    dest="reference",
+    required=True,
+    help="Path to marker by cell type reference file, should end in tsv",
+)
+parser.add_argument(
+    "-s",
+    "--seed",
+    dest="seed",
+    type=int,
+    default=None,
+    help="Random seed to set for scvi.",
+)
+parser.add_argument(
+    "-t",
+    "--threads",
+    dest="threads",
+    default=1,
+    type=int,
+    help="Number of threads to use when training the CellAssign model",
+)
 
 args = parser.parse_args()
 
@@ -53,7 +68,9 @@ file_ext = re.compile(r"\.hdf5$|.h5$", re.IGNORECASE)
 if not os.path.exists(args.input_hdf5_file):
     raise FileExistsError("--input_hdf5_file file not found.")
 elif not file_ext.search(args.input_hdf5_file):
-    raise ValueError("--input_hdf5_file must end in either .hdf5 or .h5 and contain a processed AnnData object.")
+    raise ValueError(
+        "--input_hdf5_file must end in either .hdf5 or .h5 and contain a processed AnnData object."
+    )
 
 # check that marker file exists and make sure its a tsv
 if not os.path.exists(args.reference):
@@ -64,7 +81,7 @@ if not args.output_predictions.endswith(".tsv"):
     raise ValueError("--output_predictions must provide a file path ending in tsv")
 
 # read in references as marker gene tables
-ref_matrix = pd.read_csv(args.reference, sep = "\t", index_col='ensembl_id')
+ref_matrix = pd.read_csv(args.reference, sep="\t", index_col="ensembl_id")
 
 # file path to annotated sce
 annotated_adata = adata.read_h5ad(args.input_hdf5_file)
@@ -85,7 +102,7 @@ scvi.external.CellAssign.setup_anndata(subset_adata, size_factor_key="size_facto
 model = CellAssign(subset_adata, ref_matrix)
 model.train()
 predictions = model.predict()
-predictions['barcode'] = subset_adata.obs_names
+predictions["barcode"] = subset_adata.obs_names
 
 # write out predictions as tsv
-predictions.to_csv(args.output_predictions, sep = "\t", index = False)
+predictions.to_csv(args.output_predictions, sep="\t", index=False)
--- get_refs.py
+++ get_refs.py
@@ -20,34 +20,51 @@ import json
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--refdir", type=str,
-                    default="scpca-references",
-                    help = "destination directory for downloaded reference files")
-parser.add_argument("--paramfile", type=str,
-                    default="localref_params.yaml",
-                    help = "path to nextflow param file to write (default: `localref_params.yaml`)")
-parser.add_argument("--overwrite_refs",
-                    action = "store_true",
-                    help = "replace previously downloaded files")
-parser.add_argument("-r", "--revision", type=str,
-                    default="main",
-                    metavar = "vX.X.X",
-                    help = "tag for a specific workflow version (defaults to latest revision)")
-parser.add_argument("--star_index",
-                    action = "store_true",
-                    help = "get STAR index (required for genetic demultiplexing)")
-parser.add_argument("--cellranger_index",
-                    action = "store_true",
-                    help = "get Cell Ranger index (required for spatial data)")
-parser.add_argument("--docker",
-                    action = "store_true",
-                    help = "pull and cache images for docker")
-parser.add_argument("--singularity",
-                    action = "store_true",
-                    help = "pull and cache images for singularity")
-parser.add_argument("--singularity_dir", type=str,
-                    default = "singularity",
-                    help = "directory to store singularity image files (default: `singularity`)")
+parser.add_argument(
+    "--refdir",
+    type=str,
+    default="scpca-references",
+    help="destination directory for downloaded reference files",
+)
+parser.add_argument(
+    "--paramfile",
+    type=str,
+    default="localref_params.yaml",
+    help="path to nextflow param file to write (default: `localref_params.yaml`)",
+)
+parser.add_argument(
+    "--overwrite_refs", action="store_true", help="replace previously downloaded files"
+)
+parser.add_argument(
+    "-r",
+    "--revision",
+    type=str,
+    default="main",
+    metavar="vX.X.X",
+    help="tag for a specific workflow version (defaults to latest revision)",
+)
+parser.add_argument(
+    "--star_index",
+    action="store_true",
+    help="get STAR index (required for genetic demultiplexing)",
+)
+parser.add_argument(
+    "--cellranger_index",
+    action="store_true",
+    help="get Cell Ranger index (required for spatial data)",
+)
+parser.add_argument(
+    "--docker", action="store_true", help="pull and cache images for docker"
+)
+parser.add_argument(
+    "--singularity", action="store_true", help="pull and cache images for singularity"
+)
+parser.add_argument(
+    "--singularity_dir",
+    type=str,
+    default="singularity",
+    help="directory to store singularity image files (default: `singularity`)",
+)
 args = parser.parse_args()
 
 # scpca-nf resource urls
@@ -59,11 +76,13 @@ containerfile_url = f"https://raw.githubusercontent.com/AlexsLemonade/scpca-nf/{
 # download reference file
 print("Getting list of required reference files")
 try:
-    ref_file =  urllib.request.urlopen(reffile_url)
+    ref_file = urllib.request.urlopen(reffile_url)
     json_file = urllib.request.urlopen(refjson_url)
 except urllib.error.URLError as e:
     print(e.reason)
-    print(f"The file download failed for {reffile_url} and/or {refjson_url}; please check the URLs for errors")
+    print(
+        f"The file download failed for {reffile_url} and/or {refjson_url}; please check the URLs for errors"
+    )
     print(f"Is `{args.revision}` a valid release tag?")
     exit(1)
 
@@ -75,21 +94,21 @@ ref_re = re.compile(r'(?P<id>.+?)\s*=\s*([\'"])(?P<loc>.+)\2')
 for line in ref_file:
     match = ref_re.search(line.decode())
     if match:
-        refs[match.group('id')] = match.group('loc')
+        refs[match.group("id")] = match.group("loc")
 
 # regular expressions for parameter expansion
-root_re = re.compile(r'\$\{?(params.)?ref_rootdir\}?$')
+root_re = re.compile(r"\$\{?(params.)?ref_rootdir\}?$")
 
 # get root location
 # split out protocol from the root URI
-root_parts = refs.get("ref_rootdir").split('://', maxsplit = 1)
-if root_parts[0] == 's3':
+root_parts = refs.get("ref_rootdir").split("://", maxsplit=1)
+if root_parts[0] == "s3":
     # if S3, convert bucket path to https:// url
-    bucket_path = root_parts[1].split("/", maxsplit = 1)
+    bucket_path = root_parts[1].split("/", maxsplit=1)
     url_root = f"https://{bucket_path[0]}.s3.amazonaws.com"
     if len(bucket_path) > 1:
         url_root += f"/{bucket_path[1]}"
-elif root_parts[0] in ['http', 'https', 'ftp']:
+elif root_parts[0] in ["http", "https", "ftp"]:
     # otherwise, just get the location
     url_root = refs.get("ref_rootdir")
 else:
@@ -98,20 +117,17 @@ else:
 
 # single-file references
 # the keys here are the param variables we will be downloading
-ref_keys =[
+ref_keys = [
     "ref_fasta",
     "ref_fasta_index",
     "ref_gtf",
     "mito_file",
     "t2g_3col_path",
-    "t2g_bulk_path"
+    "t2g_bulk_path",
 ]
 
 # param variables that are salmon index directories
-salmon_keys = [
-    "splici_index",
-    "salmon_bulk_index"
-]
+salmon_keys = ["splici_index", "salmon_bulk_index"]
 
 # salmon index files within index dir (must be downloaded individually through http)
 salmon_index_files = [
@@ -129,7 +145,7 @@ salmon_index_files = [
     "reflengths.bin",
     "refseq.bin",
     "seq.bin",
-    "versionInfo.json"
+    "versionInfo.json",
 ]
 
 # star index files within index dir (must be downloaded individually through http)
@@ -149,7 +165,7 @@ star_index_files = [
     "sjdbInfo.txt",
     "sjdbList.fromGTF.out.tab",
     "sjdbList.out.tab",
-    "transcriptInfo.tab"
+    "transcriptInfo.tab",
 ]
 
 # Cell Ranger index files within index dir (must be downloaded individually through http)
@@ -172,7 +188,7 @@ cr_index_files = [
     "star/sjdbInfo.txt",
     "star/sjdbList.fromGTF.out.tab",
     "star/sjdbList.out.tab",
-    "star/transcriptInfo.tab"
+    "star/transcriptInfo.tab",
 ]
 
 # create a list of all paths for all assemblies listed in reference json file
@@ -185,13 +201,17 @@ for assembly_refs in json_paths.values():
     # get paths to salmon directories and add individual files
     sa_dir = [Path(assembly_refs.get(k)) for k in salmon_keys]
     for dir in sa_dir:
-        ref_paths += [dir / f for f in salmon_index_files] # add salmon files
+        ref_paths += [dir / f for f in salmon_index_files]  # add salmon files
 
     # if cellranger and star index's are asked for, get individual files
     if args.cellranger_index:
-        ref_paths += [Path(assembly_refs.get("cellranger_index")) / f for f in cr_index_files] # add cellranger files
+        ref_paths += [
+            Path(assembly_refs.get("cellranger_index")) / f for f in cr_index_files
+        ]  # add cellranger files
     if args.star_index:
-        ref_paths += [Path(assembly_refs.get("star_index")) / f for f in star_index_files] # add star index files
+        ref_paths += [
+            Path(assembly_refs.get("star_index")) / f for f in star_index_files
+        ]  # add star index files
 
 # barcode paths are still kept in the reference config file, so add those separately
 # barcode files on S3 within the barcode_dir (must be downloaded individually through http)
@@ -200,7 +220,7 @@ barcode_files = [
     "737K-august-2016.txt",
     "cellranger_mit_license.txt",
     "visium-v1.txt",
-    "visium-v2.txt"
+    "visium-v2.txt",
 ]
 barcode_dir = Path(refs.get("barcode_dir"))
 if root_re.match(barcode_dir.parts[0]):
@@ -215,32 +235,38 @@ for path in ref_paths:
         continue
     print(f"Getting {path}")
     # make parents
-    outfile.parent.mkdir(exist_ok=True, parents = True)
+    outfile.parent.mkdir(exist_ok=True, parents=True)
     # download and write
     file_url = f"{url_root}/{path}"
     try:
         urllib.request.urlretrieve(file_url, outfile)
     except urllib.error.URLError as e:
         print(e.reason)
-        print(f"The file download failed for {file_url}; please check the URL for errors",
-              file = sys.stderr)
+        print(
+            f"The file download failed for {file_url}; please check the URL for errors",
+            file=sys.stderr,
+        )
         exit(1)
-print("Done with reference file downloads\n"
-      f"Reference files can be found at '{args.refdir}'\n")
+print(
+    "Done with reference file downloads\n"
+    f"Reference files can be found at '{args.refdir}'\n"
+)
 
 # write param file if requested
 if args.paramfile:
     pfile = Path(args.paramfile)
     # check if paramfile exists & move old if needed
     if pfile.exists():
-        print(f"A file already exists at `{pfile}`; renaming previous file to `{pfile.name}.bak`")
+        print(
+            f"A file already exists at `{pfile}`; renaming previous file to `{pfile.name}.bak`"
+        )
         shutil.move(pfile, str(pfile) + ".bak")
     # create parameter dictionary
-    nf_params = {
-        'ref_rootdir': os.path.abspath(args.refdir)
-    }
-    with open(pfile, 'w') as f:
-        f.write("# local nextflow reference file parameters, generated by `get_refs.py`\n\n")
+    nf_params = {"ref_rootdir": os.path.abspath(args.refdir)}
+    with open(pfile, "w") as f:
+        f.write(
+            "# local nextflow reference file parameters, generated by `get_refs.py`\n\n"
+        )
         for key, value in nf_params.items():
             f.write(f"{key}: {value}\n")
 
@@ -249,10 +275,12 @@ if args.singularity or args.docker:
     print("Getting list of required containers")
     containers = {}
     try:
-        container_file =  urllib.request.urlopen(containerfile_url)
+        container_file = urllib.request.urlopen(containerfile_url)
     except urllib.error.URLError as e:
         print(e.reason)
-        print(f"The file download failed for {container_url}; please check the URL for errors")
+        print(
+            f"The file download failed for {containerfile_url}; please check the URL for errors"
+        )
         print(f"Is `{args.revision}` a valid release tag?")
         exit(1)
 
@@ -261,7 +289,7 @@ if args.singularity or args.docker:
     for line in container_file:
         match = container_re.search(line.decode())
         if match:
-            containers[match.group('id')] = match.group('loc')
+            containers[match.group("id")] = match.group("loc")
 
 # pull docker images if requested
 if args.docker:
@@ -283,11 +311,10 @@ if args.singularity:
         print(image_path)
         if image_path.exists():
             image_path.unlink()
-        subprocess.run([
-            "singularity", "pull",
-            "--name", image_path,
-            f"docker://{loc}"
-        ])
+        subprocess.run(["singularity", "pull", "--name", image_path, f"docker://{loc}"])
     print("Done pulling singularity images")
-    print(f"Singularity images located at {image_dir.absolute()}; be sure to set `singularity.cacheDir` in your configuration file to this value")
+    print(
+        f"Singularity images located at {image_dir.absolute()}."
+        " Be sure to set `singularity.cacheDir` in your configuration file to this value"
+    )
     print()
