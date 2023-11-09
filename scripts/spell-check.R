80550f947c93bc4ef625ce648a47b7872271eb9d
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2023-11-09 13:55:23 -0500

    update spellcheck script

--- scripts/spell-check.R
+++ scripts/spell-check.R
@@ -11,37 +11,38 @@ root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
 # Read in dictionary
 dict_file <- file.path(root_dir, "components", "dictionary.txt")
-dictionary <- readLines(dict_file, encoding = "UTF-8")
+dictionary <- readLines(dict_file)
 
 # Add emoji to dictionary
 dictionary_plus <- c(dictionary, spelling::spell_check_text("⚠️")$word)
 
 # The only files we want to check are R Markdown and Markdown files
 files <- list.files(pattern = "\\.(Rmd|md|rmd)$", recursive = TRUE, full.names = TRUE)
+# remove any files in the work directory
+files <- files[!grepl("^./work/", files)]
 
 # Run spell check
-spelling_errors <- spelling::spell_check_files(files, ignore = dictionary_plus)
-spelling_tibble <- spelling_errors |>
+spelling_errors <- spelling::spell_check_files(files, ignore = dictionary_plus) |>
   data.frame() |>
   tidyr::unnest(cols = found) |>
-  tidyr::separate(found, into = c("file", "lines"), sep = ":") |>
-  dplyr::distinct()
-
+  tidyr::separate(found, into = c("file", "lines"), sep = ":")
 
 if (precommit) {
   if (nrow(spelling_errors) > 0) {
     cat("The following spelling errors were found:\n")
-    print(spelling_errors)
+    print(data.frame(spelling_errors))
   }
 
   # Update dictionary for future use
   updated_dict <- union(dictionary, spelling_errors$word)
   # remove empty strings
   updated_dict <- updated_dict[updated_dict != ""]
-  writeLines(sort(updated_dict), dict_file)
+  # case insensitive sort
+  updated_dict <- updated_dict[order(tolower(updated_dict))]
+  writeLines(updated_dict, dict_file)
 } else {
   # Print out how many spell check errors
-  write(nrow(spelling_tibble), stdout())
+  write(nrow(spelling_errors), stdout())
   # Save spell errors to file temporarily
-  readr::write_tsv(spelling_tibble, "spell_check_errors.tsv")
+  readr::write_tsv(spelling_errors, "spell_check_errors.tsv")
 }
