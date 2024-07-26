83045190dfc1687a297800e87a06478076a1cf98
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-07-26 14:35:34 -0400

    add variable gene conversion and PCA metadata

--- bin/move_counts_anndata.py
+++ bin/move_counts_anndata.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
+"""
+This script takes an AnnData object and checks for the `logcounts` in layers.
+If present, `logcounts` is moved to `X` and `X` (which has the raw counts) is moved to `raw.X`
 
-# This script takes an AnnData object and checks for the `logcounts`
-# in layers. If present, `logcounts` is moved to `X` and `X` (which has the raw counts)
-# is moved to `raw.X`
-# In addition, any DataFrames in `obsm` are conerted to ndarrays
-
+In addition, any DataFrames in `obsm` are conerted to ndarrays, highly variable genes are converted to a `var` column.
+If a `pca_meta_file` is provided, PCA variance statistics and standard creation values are in the format expected by scanpy.
+"""
 
 import argparse
 import os
 import re
 
-import anndata as adata
+import anndata
 import pandas as pd
 
 parser = argparse.ArgumentParser()
@@ -28,6 +29,12 @@ parser.add_argument(
     action="store_false",
     help="Output an uncompressed HDF5 file",
 )
+parser.add_argument(
+    "--pca_meta_file",
+    dest="pca_meta_file",
+    required=False,
+    help="Path to file with a table of variance explained by each PCA component",
+)
 
 args = parser.parse_args()
 
@@ -39,25 +46,55 @@ if not os.path.exists(args.anndata_file):
     raise FileExistsError("`input_anndata` does not exist.")
 elif not file_ext.search(args.anndata_file):
     raise ValueError(
-        "--input_anndata must end in either .hdf5, .h5, or .h5ad, and contain a processed AnnData object."
+        "input_anndata must end in either .hdf5, .h5, or .h5ad, and contain a processed AnnData adata."
     )
 
+# if there is a pca_meta_file, check that it exists and has a tsv extension
+if args.pca_meta_file:
+    if not os.path.exists(args.pca_meta_file):
+        raise FileExistsError("`pca_meta_file` does not exist.")
+    elif not args.pca_meta_file.endswith(".tsv"):
+        raise ValueError("`pca_meta_file` must end in .tsv.")
+
 # read in anndata
-object = adata.read_h5ad(args.anndata_file)
+adata = anndata.read_h5ad(args.anndata_file)
 
 # if logcounts is present
-if "logcounts" in object.layers:
-    # move X to raw.X by creating the raw object
-    object.raw = object
+if "logcounts" in adata.layers:
+    # move X to raw.X by creating the raw adata
+    adata.raw = adata
     # move logcounts to X and rename
-    object.X = object.layers["logcounts"]
-    object.uns["X_name"] = "logcounts"
-    del object.layers["logcounts"]
+    adata.X = adata.layers["logcounts"]
+    adata.uns["X_name"] = "logcounts"
+    del adata.layers["logcounts"]
 
 # convert DataFrames in obsm to ndarrays
-for key, value in object.obsm.items():
+for key, value in adata.obsm.items():
     if isinstance(value, pd.DataFrame):
-        object.obsm[key] = value.to_numpy()
+        adata.obsm[key] = value.to_numpy()
 
-# export object
-object.write_h5ad(args.anndata_file, compression="gzip" if args.compress else None)
+# convert highly variable genes to a column
+adata.var["highly_variable"] = adata.var.gene_ids.isin(
+    adata.uns["highly_variable_genes"]
+)
+
+# add pca adata to uns if pca_meta_file is provided in the format created by scanpy
+if args.pca_meta_file:
+    pca_meta = pd.read_csv(args.pca_meta_file, sep="\t", index_col=0)
+    if "variance" not in pca_meta.columns or "variance_ratio" not in pca_meta.columns:
+        raise ValueError(
+            "`pca_meta_file` must contain columns `variance` and `variance_ratio`"
+        )
+    pca_object = {
+        "param": {
+            "zero_center": True,
+            "use_highly_variable": True,
+            "mask_var": "highly_variable",
+        },
+        "variance": pca_meta["variance"].to_numpy(),
+        "variance_ratio": pca_meta["variance_ratio"].to_numpy(),
+    }
+    adata.uns["pca"] = pca_object
+
+# export adata
+adata.write_h5ad(args.anndata_file, compression="gzip" if args.compress else None)
