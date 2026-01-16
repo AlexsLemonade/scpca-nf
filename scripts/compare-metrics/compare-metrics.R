8b923b9fea0d386abf613a4956b7c7ccbc84ca92
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2026-01-16 14:54:27 -0500

    Update metrics paths and add early error check

--- scripts/compare-metrics/compare-metrics.R
+++ scripts/compare-metrics/compare-metrics.R
@@ -14,13 +14,13 @@ option_list <- list(
   make_option(
     c("-r", "--ref_s3"),
     type = "character",
-    default = "s3://nextflow-ccdl-results/scpca-prod/results",
+    default = "s3://ccdl-scpca-results-prod-997241705947-us-east-1/results",
     help = "S3 URI for the bucket and prefix for the reference files"
   ),
   make_option(
     c("-c", "--comp_s3"),
     type = "character",
-    default = "s3://nextflow-ccdl-results/scpca-staging/results",
+    default = "s3://ccdl-scpca-results-staging-997241705947-us-east-1/results",
     help = "S3 URI for the bucket and prefix for the comparison S3 files"
   ),
   make_option(
@@ -32,8 +32,8 @@ option_list <- list(
   make_option(
     c("-o", "--output_file"),
     type = "character",
-    default = "compare-metrics.html",
-    help = "Output file name for the rendered report"
+    default = "",
+    help = "Output file name for the rendered report. If not provided, a name will be generated with the current date and project ID(s)."
   ),
   make_option(
     c("--use_cache"), # temporary option for testing
@@ -55,10 +55,34 @@ stopifnot(
 # split project ids (flexibly by commas, semicolons, and/or whitespace)
 project_ids <- stringr::str_split_1(opt$project_id, "[,;\\s]+")
 
+if (opt$output_file != "") {
+  outfile <- opt$output_file
+} else {
+  timestamp <- format(Sys.time(), "%Y-%m-%d")
+  outfile <- paste(
+    timestamp,
+    paste(project_ids, collapse = "-"),
+    "metrics_comparison.html",
+    sep = "_"
+  )
+}
+
+# check that S3 paths are reachable
+if (!(s3fs::s3_dir_exists(opt$ref_s3) && s3fs::s3_dir_exists(opt$comp_s3))) {
+  stop(
+    "Cannot access S3 location: ",
+    opt$ref_s3,
+    "and/or ",
+    opt$comp_s3,
+    "\n",
+    "Please check that the paths exist and you have appropriate AWS credentials."
+  )
+}
+
 rmarkdown::render(
   file.path(script_dir, "compare-metrics-template.rmd"),
-  output_file = basename(opt$output_file),
-  output_dir = dirname(opt$output_file),
+  output_file = basename(outfile),
+  output_dir = dirname(outfile),
   params = list(
     reference_s3 = opt$ref_s3,
     comparison_s3 = opt$comp_s3,
