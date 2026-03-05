d74c3a4504c44bd29f4cead8f58fafdf1c6e8d99
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2026-03-05 09:45:46 -0500

    update publishing function for new fields based on technologies

--- bin/generate_spaceranger_metadata.R
+++ bin/generate_spaceranger_metadata.R
@@ -143,6 +143,24 @@ metrics_summary <- readr::read_csv(opt$metrics_summary_file)
 # read in versions file
 spaceranger_versions <- jsonlite::read_json(opt$spaceranger_versions_file)
 
+# determine column names for exporting metrics, which depends on technology
+# TODO: visium -> visium1
+if (opt$technology %in% c("visium", "visium_hd_3prime")) {
+  reads_mapped_column <- "Reads Mapped Confidently to Genome"
+} else {
+  # visium2 and visium_hd
+  reads_mapped_column <- "Reads Mapped Confidently to Probe Set"
+}
+
+if (opt$technology %in% c("visium_hd", "visium_hd_3prime")) {
+  tissue_spots_column <- "Number of Squares Under Tissue 2 µm"
+  tissue_colname <- "tissue_squares"
+} else {
+  # visium2 and visium1
+  tissue_spots_column <- "Number of Spots Under Tissue"
+  tissue_colname <- "tissue_spots"
+}
+
 # compile metadata list
 metadata_list <- list(
   library_id = opt$library_id,
@@ -152,8 +170,8 @@ metadata_list <- list(
   filtered_spots = nrow(filtered_barcodes),
   unfiltered_spots = nrow(unfiltered_barcodes),
   total_reads = metrics_summary$`Number of Reads`,
-  mapped_reads = metrics_summary$`Reads Mapped Confidently to Genome`,
-  tissue_spots = metrics_summary$`Number of Spots Under Tissue`,
+  mapped_reads = metrics_summary[[reads_mapped_column]],
+  temp_tissue_colname = metrics_summary[[tissue_spots_column]],
   genome_assembly = opt$genome_assembly,
   mapping_index = opt$index_filename,
   date_processed = lubridate::format_ISO8601(lubridate::now(tzone = "UTC"), usetz = TRUE),
@@ -162,7 +180,14 @@ metadata_list <- list(
   workflow_version = opt$workflow_version,
   workflow_commit = opt$workflow_commit
 ) |>
-  purrr::map(~ if (is.null(.)) NA else .) # convert any NULLS to NA
+  # convert any NULLS to NA
+  purrr::map(\(x){
+    ifelse(is.null(x), NA, x)
+  })
+
+# update the tissue column name
+names(metadata_list)[names(metadata_list) == "temp_tissue_colname"] <- tissue_colname
+
 
 # Output metadata as JSON
 jsonlite::write_json(metadata_list, path = opt$metadata_json, auto_unbox = TRUE)
